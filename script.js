6aa1735778cfbb26e276a8a1ee2d3f6b9740cc7f
barriertrustee <quangminha061120075a3@gmail.com>
2021-08-19 22:57:18 +0700

    commit on 'origin/QMs'

--- script.js
+++
@@ -1,9 +0,0 @@
-let header = document.querySelector('header');
-let firstImg = document.querySelector('img');
-let styleHeader = getComputedStyle(header);
-let styleFirstImg = getComputedStyle(firstImg);
-header.style.height = styleFirstImg.height;
-let body = document.querySelector('body');
-window.addEventListener('resize', e => {
-    header.style.height = styleFirstImg.height;
-})
